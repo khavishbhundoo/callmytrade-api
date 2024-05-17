36dafeef2fcb2f6b3004e8a00fe1ac307bbcd611
Khavish Anshudass Bhundoo <khavofficial@gmail.com>
2024-05-17 19:09:45 +0400

    refactoring

--- src/CallMyTrade.Api/Program.cs
+++ src/CallMyTrade.Api/Program.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using CallMyTrade.Middleware;
@@ -8,6 +9,7 @@ using Core.CallMyTrade.Services;
 using Core.CallMyTrade.Tradingview;
 using FluentValidation;
 using Serilog;
+using IPNetwork = Microsoft.AspNetCore.HttpOverrides.IPNetwork;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -40,8 +42,38 @@ builder.Services.AddHttpClient();
 
 //Add options
 builder.Services.AddOptions();
-builder.Services.AddOptions<ForwardedHeadersOptions>()
-    .BindConfiguration("ForwardedHeadersOptions");
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    var knownNetworks = new List<IPDetails>();
+    var knownProxies = new List<IPDetails>();
+    builder.Configuration.GetSection("ForwardedHeadersOptions").Bind(options);
+    builder.Configuration.GetSection("ForwardedHeadersOptions:KnownNetworks").Bind(knownNetworks);
+    builder.Configuration.GetSection("ForwardedHeadersOptions:KnownProxies").Bind(knownProxies);
+
+    if (knownNetworks.Count == 0)
+    {
+        options.KnownNetworks.Clear();
+    }
+    else
+    {
+        foreach (var ip in knownNetworks.Select(knownNetwork => IPNetwork2.Parse(knownNetwork.ToString())))
+        {
+            options.KnownNetworks.Add(new IPNetwork(ip.Network, ip.Cidr));
+        }
+    }
+    
+    if (knownProxies.Count == 0)
+    {
+        options.KnownProxies.Clear();
+    }
+    else
+    {
+        foreach (var ip in knownProxies.Select(knownProxy => IPNetwork2.Parse(knownProxy.ToString())))
+        {
+            options.KnownProxies.Add(ip.Network);
+        }
+    }
+});
 builder.Services.AddOptions<CallMyTradeOptions>()
     .BindConfiguration("CallMyTrade")
     .ValidateFluently()
