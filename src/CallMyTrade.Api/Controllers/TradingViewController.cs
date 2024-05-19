2b162cc491f010593632fa164b6a92cdb8f3b8d3
Khavish Anshudass Bhundoo <khavishbhundoo@users.noreply.github.com>
2024-05-19 13:40:32 +0400

    feat: enrich logs with request & response (#69)
    * feat: enrich logs with request & response
    
    * refactor: add route at controller level

--- src/CallMyTrade.Api/Controllers/TradingViewController.cs
+++ src/CallMyTrade.Api/Controllers/TradingViewController.cs
@@ -5,30 +5,33 @@ using Core.CallMyTrade.Tradingview;
 using FluentValidation;
 using Light.GuardClauses;
 using Microsoft.AspNetCore.Mvc;
+using Serilog;
 
 namespace CallMyTrade.Controllers;
 
+[Route(Constants.TradingViewWebhookPath)]
 public sealed class TradingViewController : ControllerBase
 {
     private readonly IValidator<TradingViewRequest> _validator;
     private readonly IPhoneCallHandler _phoneCallHandler;
     private readonly TimeProvider _timeProvider;
+    private readonly IDiagnosticContext _diagnosticContext;
 
     public TradingViewController(
         IValidator<TradingViewRequest> validator,
         IPhoneCallHandler phoneCallHandler, 
-        TimeProvider timeProvider)
+        TimeProvider timeProvider, 
+        IDiagnosticContext diagnosticContext)
     {
-        
         _validator = validator.MustNotBeNull();
         _phoneCallHandler = phoneCallHandler.MustNotBeNull();
         _timeProvider = timeProvider.MustNotBeNull();
+        _diagnosticContext = diagnosticContext.MustNotBeNull();
     }
 
     [HttpPost]
     [Consumes("application/json")]
     [Produces("application/json")]
-    [Route(Constants.TradingViewWebhookPath)]
     [ProducesResponseType(typeof(PhoneCallResponse), StatusCodes.Status201Created)]
     [ProducesResponseType(typeof(FailedResponse), StatusCodes.Status403Forbidden)]
     [ProducesResponseType(typeof(FailedResponse), StatusCodes.Status415UnsupportedMediaType)]
@@ -51,10 +54,13 @@ public sealed class TradingViewController : ControllerBase
                 });
             }
 
-            return StatusCode(StatusCodes.Status422UnprocessableEntity, new FailedResponse()
+            var failedResponse = new FailedResponse()
             {
                 ValidationErrors = validationErrors
-            });
+            };
+            _diagnosticContext.Set("FailedResponse", failedResponse, true);
+            
+            return StatusCode(StatusCodes.Status422UnprocessableEntity, failedResponse);
         }
 
         string result;
@@ -67,13 +73,23 @@ public sealed class TradingViewController : ControllerBase
         }
         catch (Exception e)
         {
-            return StatusCode(StatusCodes.Status403Forbidden, new FailedResponse()
+            var failedResponse = new FailedResponse()
             {
                 ExceptionMessage = e.Message,
                 ExceptionStackTrace = e.StackTrace
-            });
+            };
+            _diagnosticContext.Set("FailedResponse", failedResponse, true);
+            return StatusCode(StatusCodes.Status403Forbidden, failedResponse);
         }
+
+        var phoneCallResponse = new PhoneCallResponse()
+        {
+            CallResponse = result,
+            UtcDateTime = _timeProvider.GetUtcNow().UtcDateTime
+        };
         
+        _diagnosticContext.Set("PhoneCallResponse", phoneCallResponse, true);
+
         return StatusCode(StatusCodes.Status201Created, new PhoneCallResponse()
         {
             CallResponse = result,
--- test/CallMyTrade.Api.UnitTests/TradingViewControllerTests.cs
+++ test/CallMyTrade.Api.UnitTests/TradingViewControllerTests.cs
@@ -5,9 +5,11 @@ using Core.CallMyTrade.Tradingview;
 using FluentValidation;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Time.Testing;
 using NSubstitute;
 using NSubstitute.ExceptionExtensions;
+using Serilog;
 using Shouldly;
 
 namespace CallMyTrade.Api.UnitTests;
@@ -18,13 +20,15 @@ public class TradingViewControllerTests
     private readonly IPhoneCallHandler _phoneCallHandler;
     private readonly TradingViewController _sut;
     private readonly TimeProvider _timeProvider;
+    private readonly IDiagnosticContext _diagnosticContext;
 
     public TradingViewControllerTests()
     {
         _validator = new TradingViewRequestValidator();
         _phoneCallHandler = Substitute.For<IPhoneCallHandler>();
         _timeProvider = new FakeTimeProvider(startDateTime: DateTimeOffset.UtcNow);
-        _sut = new TradingViewController(_validator, _phoneCallHandler, _timeProvider);
+        _diagnosticContext = Substitute.For<IDiagnosticContext>();
+        _sut = new TradingViewController(_validator, _phoneCallHandler, _timeProvider, _diagnosticContext);
     }
 
     [Fact]
@@ -45,7 +49,9 @@ public class TradingViewControllerTests
         
         // Act
         var result = await _sut.TradingViewAsync(tradingViewRequest, CancellationToken.None);
+        //Assert
         _phoneCallHandler.Received(1).HandleCallPhoneAsync(callRequest, CancellationToken.None);
+        _diagnosticContext.Received(1).Set("PhoneCallResponse", Arg.Any<PhoneCallResponse>(), true);
         result.ShouldNotBeNull();
         var objectResult = result as ObjectResult;
         objectResult.ShouldNotBeNull();
@@ -74,7 +80,9 @@ public class TradingViewControllerTests
         
         // Act
         var result = await _sut.TradingViewAsync(tradingViewRequest, CancellationToken.None);
+        //Assert
         _phoneCallHandler.Received(1).HandleCallPhoneAsync(callRequest, CancellationToken.None);
+        _diagnosticContext.Received(1).Set("FailedResponse", Arg.Any<FailedResponse>(), true);
         result.ShouldNotBeNull();
         var objectResult = result as ObjectResult;
         objectResult.ShouldNotBeNull();
