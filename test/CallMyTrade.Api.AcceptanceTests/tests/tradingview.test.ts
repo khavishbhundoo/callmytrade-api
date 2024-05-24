a8ee3bc2027ba9b80d8d6d674b4f0d45909ec725
Khavish Anshudass Bhundoo <khavofficial@gmail.com>
2024-05-24 22:37:53 +0400

    add test case for failed response for text input

--- test/CallMyTrade.Api.AcceptanceTests/tests/tradingview.test.ts
+++ test/CallMyTrade.Api.AcceptanceTests/tests/tradingview.test.ts
@@ -9,6 +9,15 @@ const schema = z.object({
     utc_date_time: z.string(),
 });
 
+const errorSchema = z.object({
+    error_code: z.string(),
+    error_message: z.string(),
+});
+
+const failedSchema = z.object({
+    validation_errors: z.array(errorSchema),
+});
+
 const ENDPOINT = '/webhook/tradingview';
 
 describe(`POST successful request ${HOST}${ENDPOINT} with plain text`, () => {
@@ -66,5 +75,35 @@ describe(`POST successful request ${HOST}${ENDPOINT} with JSON`, () => {
 
     test('Should have valid body schema', () => {
         expect(() => schema.parse(body)).not.toThrowError();
+    });
+});
+
+
+describe(`POST failed request ${HOST}${ENDPOINT} with missing message as plaintext`, () => {
+    let response: Response;
+    let body: { [key: string]: unknown };
+
+    beforeAll(async () => {
+        const url = `${HOST}${ENDPOINT}`;
+        response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'text/plain'
+            },
+            body: ''
+        });
+        body = await response.json();
+    }, BEFORE_ALL_TIMEOUT);
+
+    test('Should have response status 422', () => {
+        expect(response.status).toBe(422);
+    });
+
+    test('Should have content-type = application/json', () => {
+        expect(response.headers.get('Content-Type')).toBe('application/json; charset=utf-8');
+    });
+
+    test('Should have valid body schema', () => {
+        expect(() => failedSchema.parse(body)).not.toThrowError();
     });
 });
